ef8a6435494a48a300c955d3ea12973dc01c492a
Gui-dev <adrianon10@gmail.com>
2021-10-04 17:57:40 -0300

    feat: add component Header

--- src/components/Header/index.tsx
+++ src/components/Header/index.tsx
@@ -1,9 +1,17 @@
+import { RiShoppingCartLine, RiStore3Line } from 'react-icons/ri'
+
 import { Container } from './style'
 
 export const Header = () => {
   return (
     <Container>
-      <h1>Header</h1>
+      <a href="/" className="home">
+        <h1>MyStore <span>.</span></h1> <RiStore3Line size={30} color="#FFF"/>
+      </a>
+
+      <a href="/cart" className="cart">
+        <RiShoppingCartLine size={30} color="#FFF"/>
+      </a>
     </Container>
   )
 }
--- src/components/Header/style.ts
+++ src/components/Header/style.ts
@@ -1,5 +1,40 @@
 import styled from 'styled-components'
 
 export const Container = styled.div`
-  
+  display: flex;
+  align-items: center;
+  justify-content: space-between;
+  height: 7rem;
+  margin: 0 10%;
+  margin-top: 2rem;
+
+  .home {
+    display: flex;
+    align-items: center;        
+    text-decoration: none;
+    
+    h1 {
+      font-size: 2.5rem;
+      color: #FFF;
+      margin-right: 1rem;
+      
+      span {
+        font-size: 4rem;
+        color: yellow;
+      }
+    }
+
+    &:hover svg {
+      fill: yellow
+    }
+  }
+
+  .cart {
+    display: flex;
+    align-items: center;  
+
+    &:hover svg {
+      fill: yellow
+    }
+  }
 `
