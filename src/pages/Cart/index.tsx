b84edb9b4c051e5794330d284df2de118d8972ab
Gui-dev <adrianon10@gmail.com>
2021-10-05 02:12:16 -0300

    feat: add Cart component style and content

--- src/pages/Cart/index.tsx
+++ src/pages/Cart/index.tsx
@@ -1,9 +1,70 @@
-import { Container } from './style'
+import { FiTrash } from 'react-icons/fi'
+
+import { Actions, Container, Finish, Info, Table, Total } from './style'
 
 export const Cart = () => {
+  const product = {
+    id: '2',
+    createdAt: '2019-09-02T07:59:58.181Z',
+    name: 'Sleek Wooden Soap',
+    price: '430.00',
+    image: 'http://lorempixel.com/640/480/transport',
+    stock: 91260
+  }
+
   return (
     <Container>
-      <h1>Home</h1>
+      <Table>
+        <thead>
+          <tr>
+            <th></th>
+          </tr>
+        </thead>
+
+        <tbody>
+          <tr>
+            <td>
+
+              <img src={ product.image } alt={ product.name } />
+              <Info>
+                <h1>{ product.name }</h1>
+                <strong>{ product.price }</strong>
+
+                <Actions>
+                  <button>-</button>
+                  <input type="text" value="2" min="1"/>
+                  <button>+</button>
+
+                  <button className="trash">
+                    <FiTrash size={24} color="red"/>
+                  </button>
+                </Actions>
+
+              </Info>
+            </td>
+          </tr>
+
+          <tr>
+            <td>
+              <Total>
+                <span>Total</span>
+                <strong>860.00</strong>
+              </Total>
+            </td>
+          </tr>
+
+          <tr>
+            <td>
+              <Finish>
+                <button>
+                  <span>Checkout</span>
+                </button>
+              </Finish>
+            </td>
+          </tr>
+
+        </tbody>
+      </Table>
     </Container>
   )
 }
--- src/pages/Cart/style.ts
+++ src/pages/Cart/style.ts
@@ -1,5 +1,138 @@
 import styled from 'styled-components'
+import { lighten } from 'polished'
 
 export const Container = styled.div`
+  display: flex;
+  margin: 3rem 10%;
+`
+
+export const Table = styled.table`
+  width: 100%;
   
+  tbody {    
+    tr {
+      position: relative;
+      display: flex;
+      flex-direction: column;
+      padding: 1.5rem;
+      background-color: var(--gray-800);
+      margin-bottom: 2rem;
+
+      td {
+        display: flex;
+        flex-direction: row;
+
+        img {
+          margin-right: 1.5rem;
+          height: 8rem;
+          width: 8rem;
+        }
+      }
+    }
+  }
+
+`
+
+export const Info = styled.div`
+  h1 {
+    font-size: 1.6rem;
+    font-weight: bold;          
+  }
+
+  strong {
+    font-size: 2rem;
+    font-weight: bold; 
+    margin-top: 1.5rem;
+  }
+`
+
+export const Actions = styled.div`
+  display: flex;
+  align-items: center;
+  margin-top: 1rem;
+
+  button {
+    display: flex;
+    align-items: center;
+    justify-content: center;
+    font-size: 2rem;
+    color: var(--white);
+    height: 3rem;
+    width: 3rem;
+    background-color: var(--green-500);
+    border: 1px solid var(--green-500);
+    border-radius: .4rem;
+    transition: background-color 0.3s;
+    cursor: pointer;
+  }
+
+  > input {
+    display: flex;
+    align-items: center;
+    justify-content: center;
+    text-align: center;
+    height: 3rem;
+    width: 5rem;
+  }
+
+  .trash {
+    position: absolute;
+    right: 1rem;
+    background-color: transparent;
+    border: 0;
+  }
+
+`
+
+export const Total = styled.div`
+  display: flex;
+  align-items: center;
+  justify-content: space-around;
+  width: 100%;
+
+  span {
+    display: block;
+    font-size: 1.6rem;
+  }
+
+  strong {
+    font-size: 2.5rem;
+    font-weight: bold;
+    color: var(--white);
+  }
+`
+
+export const Finish = styled.div`
+  display: flex;
+  align-items: center;
+  justify-content: center;
+  width: 100%;
+
+  button {
+    display: flex;
+    align-items: center;
+    justify-content: center;
+    height: 5rem;
+    width: 100%;
+    background-color: var(--green-500);
+    border: 1px solid var(--green-500);
+    border-radius: .4rem;
+    transition: background-color 0.3s;
+    cursor: pointer;
+
+    span {
+      font-size: 2rem;
+      font-weight: bold;
+      color: var(--white);
+      margin-top: 3px;
+    }
+
+    svg {
+      margin-right: 1rem;
+    }
+
+    &:hover {
+      background-color: ${lighten(0.07, '#3EB595')};
+    }
+  }
 `
