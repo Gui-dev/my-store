d66a68e10fe46946538aecf6dfc2a24b5dd500c0
Gui-dev <adrianon10@gmail.com>
2021-10-07 19:41:41 -0300

    test: it should have a empty cart

--- src/pages/Cart/__tests__/Cart.test.tsx
+++ src/pages/Cart/__tests__/Cart.test.tsx
@@ -1,11 +1,11 @@
 import React from 'react'
 import { render, screen } from '@testing-library/react'
-
+import { BrowserRouter } from 'react-router-dom'
 import { Cart } from '..'
 
 describe('<Cart />', () => {
   it('should have a empty cart', () => {
-    render(<Cart />)
+    render(<BrowserRouter><Cart /></BrowserRouter>)
 
     expect(
       screen.getByText(/você ainda não possui produtos no seu carrinho/i)
