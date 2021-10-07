c090635faf200888a0ea7db78c87c98b798ff362
Gui-dev <adrianon10@gmail.com>
2021-10-07 14:04:37 -0300

    test: it should have a heading

---
+++ src/components/ProductList/__tests__/ProductList.test.tsx
@@ -0,0 +1,28 @@
+import React from 'react'
+import { render, screen } from '@testing-library/react'
+
+import { ProductList } from './../'
+
+const product = {
+  id: '1',
+  createdAt: '2019-09-02T12:58:54.103Z',
+  name: 'Rustic Metal Fish',
+  price: '289.00',
+  image: 'http://lorempixel.com/640/480/food',
+  stock: 65171
+}
+
+describe('<ProductList />', () => {
+  it('renders correctly', () => {
+    render(<ProductList product={product}/>)
+
+    expect(screen.getByText('Add to Cart')).toBeInTheDocument()
+  })
+
+  it('should have a heading', () => {
+    render(<ProductList product={product}/>)
+    expect(screen.getByRole('heading', {
+      name: /rustic metal fish/i
+    })).toBeInTheDocument()
+  })
+})
